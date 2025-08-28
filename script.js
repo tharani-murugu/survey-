ebbbea6b65be690cda0042e2dc977aa22ccac35a
tharani-murugu <142904674+tharani-murugu@users.noreply.github.com>
2025-08-28 11:23:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+/* 
+var a=20;
+{
+    var b=10;
+    
+}
+console.log(b);
+let student,name;
+name="thara";
+student=name;
+console.log(student); */
+/* 
+let names= `thia's my pen and you can use this "pen" that is all`;
+console.log(typeof names);
+
+let n;
+console.log(n) */
+
+/* let a='goms';
+let b='Zoms';
+let result;
+if(a>b)
+console.log(a);
+else
+    console.log(b); */
+
+/* let year=2001;
+
+if((year%4==0)&&(year%100==0)&&(year%400==0))
+    console.log("leap year");
+else
+    console.log("not leap year"); */
+
+/* let a,b;
+function multy(mul,a,b){
+    return mul(a,b);
+}
+let m=(a,b)=>a*b;
+console.log(m,2,5); */
+
+/* let userName={
+    name:'Tharani',
+    age:24
+};
+for(key in userName)
+{
+    console.log(userName[key]);
+    
+} */
+/* 
+    let a={                            //object
+        name:'thara',
+      
+    };                              
+    function User(a){                    //constractor
+        this.name =a;
+    }
+    let thara=new User('thara');       //create value in multyple time
+    let navi=new User('navi');
+    
+    console.log(thara,navi); */
+/* 
+    let l=[
+        [1,2],
+        [2,4]
+      ]
+  for(let i=0;i<l.length;i++){
+     for(let j=0;j<l[i].length;j++){
+   
+        console.log(l[i][j]);
+     }
+  } */
+
